ecc782fa6f9f1dbe2d5c93cd22cd7a6bd5960e6a
sub0ne1 <iamsubhajit.d@gmail.com>
2024-04-04 23:51:43 -0400

    Test for goralim.go is done

--- goralim_test.go
+++ goralim_test.go
@@ -20,4 +20,28 @@ func TestRefillTokens(t *testing.T){
     }
 }
 
+func TestIsAllowed(t *testing.T){
+
+    redisClient := redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+    defer redisClient.Close()
+
+    key := "test_key"
+    capacity := 10
+    refillRate := 5
+
+    tb := NewTokenBucket(key, redisClient, capacity, refillRate)
+
+    for i:=0; i < capacity; i++ {
+        tb.isAllowed()
+    }
+
+    if tb.isAllowed() != false {
+        t.Errorf("got %t, wanted %t", tb.isAllowed(), false)
+    }
+
+
+
+
+}
+
 
